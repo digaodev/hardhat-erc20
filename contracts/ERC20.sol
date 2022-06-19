12663fffcb834f15ce080b826862eac3329c67e0
Rodrigo RB <rodrigo.rb.dev@gmail.com>
2022-06-19 16:28:38 -0300

    feat: add basic wrapped token mechanism

--- contracts/ERC20.sol
+++ contracts/ERC20.sol
@@ -10,12 +10,15 @@ contract ERC20 {
     mapping(address => uint256) public balanceOf;
     mapping(address => mapping(address => uint256)) public allowance;
     event Transfer(address indexed from, address indexed to, uint256 value);
-    event Approval(address indexed owner, address indexed spender, uint256 value);
+    event Approval(
+        address indexed owner,
+        address indexed spender,
+        uint256 value
+    );
+    event Deposit(address indexed sender, uint256 amount);
+    event Withdrawal(address indexed recipient, uint256 amount);
 
-    constructor(
-        string memory _name,
-        string memory _symbol
-    ) {
+    constructor(string memory _name, string memory _symbol) {
         name = _name;
         symbol = _symbol;
 
@@ -36,9 +39,13 @@ contract ERC20 {
         return true;
     }
 
-    function transferFrom(address sender, address to, uint256 amount) external returns (bool) {
+    function transferFrom(
+        address sender,
+        address to,
+        uint256 amount
+    ) public returns (bool) {
         uint256 currentAllowance = allowance[sender][msg.sender];
-        
+
         require(currentAllowance >= amount, "ERC20: allowance exceeded");
 
         allowance[sender][msg.sender] = currentAllowance - amount;
@@ -48,15 +55,19 @@ contract ERC20 {
         return _transfer(sender, to, amount);
     }
 
-    function transfer(address to, uint256 amount) external returns (bool) {
+    function transfer(address to, uint256 amount) public returns (bool) {
         return _transfer(msg.sender, to, amount);
     }
 
-    function _transfer(address sender, address to, uint256 amount) private returns (bool) {
+    function _transfer(
+        address sender,
+        address to,
+        uint256 amount
+    ) private returns (bool) {
         require(to != address(0), "ERC20: transfer to zero address");
 
         uint256 senderBalance = balanceOf[sender];
-        
+
         require(senderBalance >= amount, "ERC20: balance exceeded");
 
         balanceOf[sender] = senderBalance - amount;
@@ -75,5 +86,32 @@ contract ERC20 {
         balanceOf[to] = balanceOf[to] + amount;
 
         emit Transfer(address(0), to, amount);
+    }
+
+    function _burn(address sender, uint256 amount) internal {
+        require(sender != address(0), "ERC20: burn to zero address");
+
+        totalSupply = totalSupply - amount;
+        balanceOf[sender] = balanceOf[sender] - amount;
+    }
+
+    function deposit() public payable {
+        _mint(msg.sender, msg.value);
+
+        emit Deposit(msg.sender, msg.value);
+    }
+
+    function withdraw(address sender, uint256 amount) public {
+        require(balanceOf[sender] >= amount, "ERC20: balance exceeded");
+
+        transfer(msg.sender, amount);
+
+        address payable recipient = payable(msg.sender);
+
+        _burn(msg.sender, amount);
+
+        recipient.transfer(amount);
+
+        emit Withdrawal(recipient, amount);
     }
 }
